77f524231d30f9da5906d9b7988d3f115a8e008f
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-11-21 23:09:42 +0700

    Implement portal controller with mouse movement

---
+++ Assets/PortalController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class PortalController : MonoBehaviour
+{
+    public VisualEffect spawningPortal;
+    public VisualEffect staticPortal;
+    public float portalRadius;
+    public float minAngularSpeed;
+
+    private bool hasPortalSpawned;
+    private bool firstClick;
+    private float targetRadius;
+    private Vector2 centerPoint;
+    private Vector2 rightVector;
+    private float oldRadian;
+
+    private void Awake()
+    {
+        hasPortalSpawned = false;
+        firstClick = true;
+        rightVector = new Vector2(1f, 0);
+        targetRadius = Screen.height * .25f;
+    }
+
+    void Update()
+    {
+        if (hasPortalSpawned)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                hasPortalSpawned = false;
+                staticPortal.Stop();
+            }
+        }
+        else
+        {
+
+            if (Input.GetMouseButton(1))
+            {
+                if (firstClick)
+                {
+                    spawningPortal.Play();
+                    firstClick = false;
+                    centerPoint = GetMousePosition();
+                }
+
+                Vector2 currentMousePos = GetMousePosition();
+                Vector2 direction = (currentMousePos - centerPoint);
+                float magnitude = direction.magnitude;
+                float radian = Mathf.Acos(Vector2.Dot(direction / magnitude, rightVector));
+
+                if (direction.y < 0)
+                {
+                    radian = Mathf.PI * 2 - radian;
+                }
+
+                float currentAngularSpeed = Mathf.Abs(radian - oldRadian) / Time.deltaTime;
+                float currentRadius = Mathf.Min((magnitude / targetRadius) * portalRadius, portalRadius);
+                int currentSpawnRate = (int)((currentAngularSpeed / minAngularSpeed) * (currentRadius / portalRadius) * 4000);
+                currentSpawnRate = Mathf.Clamp(currentSpawnRate, 500, 4000);
+
+                spawningPortal.SetFloat("ArcSequencer", RadianToArcSequencer(radian));
+                spawningPortal.SetFloat("MajorRadius", currentRadius);
+                spawningPortal.SetInt("SpawnRate", currentSpawnRate);
+
+                oldRadian = radian;
+
+                if (currentAngularSpeed >= minAngularSpeed && currentRadius == portalRadius && Input.GetMouseButtonDown(0))
+                {
+                    staticPortal.SetFloat("MajorRadius", portalRadius);
+                    staticPortal.Play();
+                    hasPortalSpawned = true;
+                    StopSpawnSequence();
+                }
+            }
+            else
+            {
+                StopSpawnSequence();
+            }
+        }
+    }
+
+    private void StopSpawnSequence()
+    {
+        firstClick = true;
+        spawningPortal.Stop();
+    }
+
+    private Vector2 GetMousePosition()
+    {
+        return new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    }
+
+    private float RadianToArcSequencer(float radian)
+    {
+        float normalizedRadian = radian / (Mathf.PI * 2);
+        normalizedRadian -= .25f;
+        if (normalizedRadian < 0)
+        {
+            normalizedRadian += 1;
+        }
+        return 1 - normalizedRadian;
+    }
+}
